f7e64fee9f205de290a2dc9d9f7c8aac6ea7d60f
N Somith Singh <ningombamsomith@gmail.com>
2023-04-30 23:30:47 +0530

    added announcement page

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+background-color: teal;
+display: flex;
+justify-content: center;
+align-items: center;
+text-align: center;
+`;
+
+const Announcement = () => {
+  return (
+    <Container>An page</Container>
+  )
+}
+
+export default Announcement
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import SearchIcon from '@mui/icons-material/Search';
-
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Badge } from '@mui/material';
 const Container = styled.div`
  height: 60px;
 `
 
 const Wrapper = styled.div`
- padding: 10px 20px;
+ padding: 0px 20px;
  display: flex;
  justify-content: space-between;
  align-items: center;
@@ -40,10 +41,19 @@ flex: 1;
 `;
 
 const Logo = styled.h1`
-
+    text-align: center;
 `;
 const Right = styled.div`
 flex: 1;
+display: flex;
+align-items: center;
+justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+font-size: 14px;
+cursor: pointer;
+margin: auto auto;
 `;
 
 const NavBar = () => {
@@ -58,7 +68,15 @@ const NavBar = () => {
                     </SearchContainer>
                 </Left>
                 <Center><Logo>HI</Logo></Center>
-                <Right>Rg</Right>
+                <Right>
+                    <MenuItem>Register</MenuItem>
+                    <MenuItem>Sign In</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlinedIcon color="action" />
+                        </Badge>
+                    </MenuItem>
+                </Right>
             </Wrapper>
         </Container>
     )
